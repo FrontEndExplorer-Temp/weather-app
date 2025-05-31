47ff7304d22be664657af05ef33c03ebc3d0b7a6
FrontEndExplorer-Temp <purushothravi7@gmail.com>
2025-05-31 08:35:48 +0530

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',  
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',  
-    path: path.resolve(__dirname, 'dist'),  
-    clean: true,  
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   devServer: {
-    static: './dist', 
-    hot: true,  
-    open: true,  
+    static: "./dist",
+    hot: true,
+    open: true,
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,  
-        use: ['style-loader', 'css-loader'],  
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/template.html',  
-      filename: 'index.html',  
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" }, // Copies assets to dist/assets
+      ],
     }),
   ],
-  mode: 'production',
+  mode: "production",
 };
